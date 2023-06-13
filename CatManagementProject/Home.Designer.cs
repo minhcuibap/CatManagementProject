9135d16123edd8214bcd550254929df9ec6a9c2a
Minh Dinh <hello@minh.com>
2023-06-13 18:20:11 +0700

    added home design, updated CatManagementDBContext

--- CatManagementProject/Home.Designer.cs
+++ CatManagementProject/Home.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Home";
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            panel3 = new Panel();
+            button2 = new Button();
+            panel4 = new Panel();
+            button3 = new Button();
+            panel5 = new Panel();
+            button4 = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.LimeGreen;
+            flowLayoutPanel1.Controls.Add(panel1);
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(panel3);
+            flowLayoutPanel1.Controls.Add(panel4);
+            flowLayoutPanel1.Controls.Add(panel5);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(207, 491);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(204, 75);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(3, 84);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(204, 65);
+            panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(-21, -4);
+            button1.Name = "button1";
+            button1.Size = new Size(254, 74);
+            button1.TabIndex = 2;
+            button1.Text = "Home";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button2);
+            panel3.Location = new Point(3, 155);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(204, 65);
+            panel3.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(-21, -4);
+            button2.Name = "button2";
+            button2.Size = new Size(254, 74);
+            button2.TabIndex = 2;
+            button2.Text = "Services";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(button3);
+            panel4.Location = new Point(3, 226);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(204, 65);
+            panel4.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(-21, -4);
+            button3.Name = "button3";
+            button3.Size = new Size(254, 74);
+            button3.TabIndex = 2;
+            button3.Text = "Cat List";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(button4);
+            panel5.Location = new Point(3, 297);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(204, 65);
+            panel5.TabIndex = 4;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(-21, -4);
+            button4.Name = "button4";
+            button4.Size = new Size(254, 74);
+            button4.TabIndex = 2;
+            button4.Text = "Log Out";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(980, 491);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Home";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Home";
+            flowLayoutPanel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private Panel panel3;
+        private Button button2;
+        private Panel panel4;
+        private Button button3;
+        private Panel panel5;
+        private Button button4;
     }
 }

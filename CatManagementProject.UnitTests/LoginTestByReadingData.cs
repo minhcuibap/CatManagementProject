f10c9e0ba1a188c3fc9efed302f6309c58c4fb60
TNKLBAH <putinkhanh112@gmail.com>
2023-07-13 10:11:52 +0700

    Test Login by Reading Data

---
+++ CatManagementProject.UnitTests/LoginTestByReadingData.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CatManagementProject.UnitTests
+{
+    [TestFixture]
+    public class LoginTestByReadingData
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var login = new Login();
+            var accountHelper = new AccountHelper();
+        }
+
+        private static IEnumerable<String[]> GetTestData()
+        {
+            using (var reader = new StreamReader("test_data.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(",");
+                    yield return values;
+                }
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetTestData))]
+        public void checkLogin_GivenRightInput_ReturnsWell(String username, String password, String expectedResult)
+        {
+            bool expectedValue = bool.Parse(expectedResult);
+
+            var login = new Login();
+            var accountHelper = new AccountHelper();
+
+            var valid = login.checkLogin(accountHelper.GetAll(), username, password);
+            bool actualValue = valid != null ? true : false;
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
